3aab3538ec0792880b0e5b0082a07cdc7cbaaee2
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-12 20:12:42 -0600

    Test setup to test useActionState

---
+++ src/app/ui/signup.tsx
@@ -0,0 +1,27 @@
+//@/app/ui/signup.tsx   
+'use client'
+
+import { useActionState } from 'react'
+import { createUser } from '@/app/actions'
+
+const initialState = {
+  message: '',
+}
+
+export function Signup() {
+  const [state, formAction] = useActionState(createUser, initialState)
+
+  return (
+    <form action={formAction}>
+      <label htmlFor="email">Email</label>
+      <input type="text" id="email" name="email" required />
+      {/* ... */}
+      <p aria-live="polite" className="sr-only">
+        {state?.message}
+      </p>
+      <button>Sign up</button>
+    </form>
+  )
+}
+
+export default Signup
